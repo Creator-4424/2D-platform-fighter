d92ec5b5903a7409accb479e0c33ba5abad635c3
Creator-4424 <lcverde1@gmail.com>
2025-05-06 16:08:18 +0200

    Initial commit

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -3,6 +3,9 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
     public CharacterController2D controller;
+    public float speed = 40f;
+    public float HorizontalMove = 0f;
+    bool jump = false;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -12,6 +15,15 @@ public class PlayerMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        HorizontalMove = Input.GetAxisRaw("Horizontal") * speed;
+        if (Input.GetButtonDown("Jump"))
+        {
+            jump = true;
+        }
+    }
+    void FixedUpdate()
+    {
+        controller.Move(HorizontalMove*Time.fixedDeltaTime, false, jump);
+        jump = false;
     }
 }
